0814a48154f38593f997b11b594ae1d17ac688c2
Kevin Macey <image28@slingshot.co.nz>
2021-04-20 03:08:01 +1200

    Haven't small connection drops
    Changed run.sh to compensate for now

--- build/nsfminer/run.sh
+++ build/nsfminer/run.sh
@@ -1,1 +1,1 @@
-./nsfminer -G --devices 1 --syslog --cl-work=256  --nocolor -P stratum2+tcp://341T2ew7f3aZafjfmhQWRhJ3VaA3pcHCCL@172.65.223.147:3353
+./nsfminer -G --display-interval=60 --retry-delay=59 --response-timeout=180 --devices 1 --syslog --cl-work=256 --nocolor -P stratum2+tcp://341T2ew7f3aZafjfmhQWRhJ3VaA3pcHCCL@172.65.223.147:3353
